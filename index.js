b05bb0340ef1aae8dce320be851ce91caf360a43
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-14 18:12:43 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Import stylesheets
 import './style.css';
 
+
 // Write Javascript code!
+var increment=0;
+var s=document.getElementById("snake");
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 function snake()
 {
-  document.getElementById(snake)
+
+  s.style.left=increment+"px";
+  increment++;
+
 }
+setInterval(snake,10)
