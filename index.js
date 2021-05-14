6d87dfe66b25e9b2122a602362487dfb1a8d9efc
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-14 17:56:37 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function snake()
+{
+  document.getElementById(snake)
+}
